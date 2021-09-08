ecc10fb14ed999957bdd3d5b49e39071bba79048
Jack Meng <error666jm@gmail.com>
2021-09-08 12:12:48 -0400

    added layout

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,52 +1,75 @@
 package main;
 
 import javax.swing.JPanel;
-import javax.swing.plaf.DimensionUIResource;
+import javax.swing.UIManager;
 import javax.swing.JFrame;
 
 import java.io.File;
 
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
 import java.net.URL;
+import java.awt.CardLayout;
 import java.awt.Component;
 import java.awt.Font;
+import java.awt.Dimension;
+import java.awt.Container;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Test implements Runnable, ActionListener {
-  protected JPanel mp;
-  protected JFrame frame;
-  protected JButton button;
-  protected JLabel main, footer_notice;
+  protected JPanel bp, mainPanel;
+  private static JFrame frame;
+  protected JButton play_btn, pause_btn;
+  protected JLabel main, header_notice;
 
   Test() {
     URL frame_icon = getClass().getResource("assets/frame-icon.png/");
+    URL play_icon = getClass().getResource("assets/play_button.png");
+    URL pause_icon = getClass().getResource("assets/pause_button.png");
+
+    Icon pause_button_ico = new ImageIcon(pause_icon);
+    Icon play_button_ico = new ImageIcon(play_icon);
     ImageIcon frame_ico = new ImageIcon(frame_icon);
 
     frame = new JFrame("Music Player Java - v1.0");
     frame.setIconImage(frame_ico.getImage());
     frame.setResizable(false);
 
-    footer_notice = new JLabel(
+    play_btn = new JButton(play_button_ico);
+    play_btn.addActionListener(this);
+
+    pause_btn = new JButton(pause_button_ico);
+    pause_btn.addActionListener(this);
+
+    header_notice = new JLabel(
         "<html><body><strong><u>Supported File Types : .mp3 & .wav</u></strong><br><center>Place files in folder: <code>/musicML/</code></center></body></html><br>");
 
-    footer_notice.setFont(new Font("Courier", Font.PLAIN, 13));
-    footer_notice.setAlignmentX(Component.CENTER_ALIGNMENT);
-    footer_notice.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+    header_notice.setFont(new Font("Courier", Font.PLAIN, 13));
+    header_notice.setAlignmentX(Component.CENTER_ALIGNMENT);
+    header_notice.setAlignmentY(Component.BOTTOM_ALIGNMENT);
 
-    mp = new JPanel();
-    mp.add(footer_notice);
-    mp.setPreferredSize(new DimensionUIResource(450, 200));
+    
 
-    frame.add(mp);
+    bp = new JPanel();
+    bp.add("1", play_btn);
+    bp.add("2", pause_btn);
+
+    bp.setPreferredSize(new Dimension(450, 200));
+
+    mainPanel = new JPanel(new CardLayout());
+    mainPanel.add(bp);
+
+    frame.add(mainPanel);
 
   }
-
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
+    UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
     File f = new File("musicML");
     if (!f.exists())
       f.mkdir();
