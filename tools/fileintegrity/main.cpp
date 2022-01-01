9aebdcedaca6190d52f3ed8f03a9359b7336e365
Jack Meng <error666jm@gmail.com>
2022-01-01 11:16:02 -0500

    `primitive` file integrity checker

---
+++ tools/fileintegrity/main.cpp
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#pragma comment(lib, "cryptlib.lib")
+#pragma comment(lib, "libeay32.lib")
+
+#define speed ios::sync_with_stdio(false)
+#define endl "\n"
+
+using namespace std;
+
+const string def_path = "/resource/";
+
+int main() {
+  speed;
+  string temp = "";
+  ifstream list("./files.txt");
+  while (getline(list, temp)) {
+    ifstream ifile;
+    ifile.open(temp);
+    if (ifile) {
+      temp = "";
+      continue;
+    } else {
+      cout << temp << endl;
+      cout << "1" << endl;
+      return 1;
+    }
+  }
+}
