e454be6271993fce6fdec2856cfb18e91516749a
Jack Meng <jackmeng0814@gmail.com>
2022-04-28 21:58:26 -0400

    `fix & update` fixed sizing and progress slider & updated to add time label

--- src/project/components/sub_components/infoview/BottomView.java
+++ src/project/components/sub_components/infoview/BottomView.java
@@ -2,6 +2,7 @@ package project.components.sub_components.infoview;
 
 import project.audio.Overseer;
 import project.constants.Size;
+import project.usables.TimeTool;
 import project.constants.ColorContent;
 
 import project.components.sub_components.infoview.SubVolumeView;
@@ -15,6 +16,7 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 
@@ -22,19 +24,22 @@ import com.goxr3plus.streamplayer.stream.StreamPlayerEvent;
 import com.goxr3plus.streamplayer.stream.StreamPlayerListener;
 
 public class BottomView extends JPanel implements StreamPlayerListener {
-  private JButton playPauseButton;
   private JSlider progressSlider;
-  private Overseer seer;
+  private JLabel timeLabel;
+  private transient Overseer seer;
   public BottomView(Overseer jf) {
     super();
     this.seer = jf;
     setLayout(new FlowLayout(FlowLayout.LEFT, 20, 15));
-    setPreferredSize(new Dimension((int) getPreferredSize().getWidth(), Size.PREV_HEIGHT - 500));
-    setMaximumSize(new Dimension((int) getPreferredSize().getWidth(), Size.PREV_HEIGHT - 530));
+    setPreferredSize(new Dimension((int) getPreferredSize().getWidth(), Size.HEIGHT - 250));
     seer.addStreamPlayerListener(this);
     setBorder(BorderFactory.createLineBorder(ColorContent.BORDER, 1, true));
     setOpaque(true);
     add(jf.getPlayPauseButton());
+
+    timeLabel = new JLabel("0:00:00 / 0:00:00");
+    add(timeLabel);
+
     progressSlider = jf.getProgressSlider();
     progressSlider.setPreferredSize(new Dimension(300, 20));
     add(progressSlider);
@@ -43,12 +48,12 @@ public class BottomView extends JPanel implements StreamPlayerListener {
   @Override
   public void opened(Object arg0, Map<String, Object> arg1) {
   }
+  
   @Override
-  public void progress(final int arg0, long arg1, byte[] arg2, Map<String, Object> arg3) {
-    long totalTimeMCS = seer.getDurationInSeconds() * 1000000L;
-    progressSlider.setValue((int) (arg1 / totalTimeMCS));
-    System.out.println(arg1 / totalTimeMCS);
-    progressSlider.setToolTipText("Progress: " + arg1 + " / " + totalTimeMCS);
+  public void progress(final int arg0, final long arg1, byte[] arg2, Map<String, Object> arg3) {
+    progressSlider.setValue((int) (((arg1 / 1000000d) / seer.getDurationInSeconds()) * 100));
+    progressSlider.setToolTipText(TimeTool.getTimeFromMS(arg1 / 1000L) + " / " + TimeTool.getTimeFromMS(seer.getDurationInSeconds() * 1000L));
+    timeLabel.setText(TimeTool.getTimeFromMS(arg1 / 1000L) + " / " + TimeTool.getTimeFromMS(seer.getDurationInSeconds() * 1000L));
   }
   @Override
   public void statusUpdated(StreamPlayerEvent arg0) {
