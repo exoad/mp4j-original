739413ec990aa04fcaf8bdea88c90d353c5e08b9
Jack Meng <jackmeng0814@gmail.com>
2022-05-13 19:23:22 -0400

    `testing-size`

--- src/project/constants/Size.java
+++ src/project/constants/Size.java
@@ -3,13 +3,10 @@ package project.constants;
 public final class Size {
   private Size() {}
 
-  public static final int WIDTH = 900;
-  public static final int HEIGHT = 375;
+  public static final int WIDTH = 920;
+  public static final int HEIGHT = 360;
 
   public static final int PREV_HEIGHT = 390;
-
-  public static final int MAX_BIGCONTAINER_WIDTH = 1300;
-  public static final int MAX_BIGCONTAINER_HEIGHT = 700;
 
   public static final int PAUSE_PLAY_BUTTON_SIZE = 25;
 
