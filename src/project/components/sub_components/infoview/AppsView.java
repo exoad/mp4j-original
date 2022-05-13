125e4ad25f2a4f1e288df567d2d5eec04f95866c
Jack Meng <jackmeng0814@gmail.com>
2022-05-13 19:35:19 -0400

    `testing-rm-thread-accesses`

--- src/project/components/sub_components/infoview/AppsView.java
+++ src/project/components/sub_components/infoview/AppsView.java
@@ -33,7 +33,6 @@ public class AppsView extends JPanel {
       @Override
       public synchronized void paintComponent(Graphics g) {
         super.paintComponent(g);
-        new Thread(() -> {
           Graphics2D g2 = (Graphics2D) g;
           g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
           g2.setColor(ColorContent.WAVE_FORM_BAR_X);
@@ -47,7 +46,6 @@ public class AppsView extends JPanel {
             }
           }
           g2.dispose();
-        }).start();
       }
     };
     pane = new JScrollPane(waveFormDisplay);
