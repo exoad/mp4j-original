6330ee61a7764bafb37c07d1f192c119217b395d
Jack Meng <jackmeng0814@gmail.com>
2022-05-28 15:11:53 -0400

    `removed` all dependence on hardcode colors removed

--- src/project/constants/ColorContent.java
+++ src/project/constants/ColorContent.java
@@ -17,13 +17,16 @@ import project.usables.ColorTool;
 public class ColorContent {
   public static final Color BORDER = ColorTool
       .hexToRGBA(ResourceFolder.pm.get(ProjectManager.KEY_BORDER_HEXCOLOR));
-  public static final Color VOLUME_SLIDER_HEARING_LOSS_FG = new Color(250, 45, 45);
+  public static final Color VOLUME_SLIDER_HEARING_LOSS_FG = ColorTool
+      .hexToRGBA(ResourceFolder.pm.get(ProjectManager.KEY_VOLUME_SLIDER_HEARING_LOSS_FG_HEX_COLOR));
   public static final Color VOLUME_SLIDER_BG = ColorTool
       .hexToRGBA(ResourceFolder.pm.get(ProjectManager.KEY_VOLUME_SLIDER_BG_HEX_COLOR));
-  public static final Color VOLUME_SLIDER_WARNING_FG = new Color(255, 170, 43);
+  public static final Color VOLUME_SLIDER_WARNING_FG = ColorTool
+      .hexToRGBA(ResourceFolder.pm.get(ProjectManager.KEY_VOLUME_SLIDER_WARNING_FG_HEX_COLOR));
   public static final Color VOLUME_SLIDER_NORMAL_FG = ColorTool.hexToRGBA(
       ResourceFolder.pm.get(ProjectManager.KEY_VOLUME_SLIDER_FG_HEX_COLOR));
-  public static final Color VOLUME_SLIDER_MUTED_FG = new Color(105, 105, 105);
+  public static final Color VOLUME_SLIDER_MUTED_FG = ColorTool
+      .hexToRGBA(ResourceFolder.pm.get(ProjectManager.KEY_VOLUME_SLIDER_MUTED_FG_HEX_COLOR));
   public static final Color PROGRESS_SLIDER_NORMAL = ColorTool.hexToRGBA(
       ResourceFolder.pm.get(ProjectManager.KEY_PROGRESS_SLIDER_NORMAL_HEX_COLOR));
   public static final Color WAVE_FORM_BAR_X = ColorTool
--- src/project/constants/ProjectManager.java
+++ src/project/constants/ProjectManager.java
@@ -11,7 +11,8 @@ import java.util.HashMap;
 public class ProjectManager {
   private ProjectManager() {
   }
-  public static final String[] EXT_RSC_FOLDERS = new String[] {"logs", "bin", "cache"};
+
+  public static final String[] EXT_RSC_FOLDERS = new String[] { "logs", "bin", "cache" };
   public static final String GITHUB_PROJECT_URL = "https://github.com/Exoad4JVM/mp4j";
   public static final boolean DEBUG_LAYOUT = false;
   public static final boolean PRODUCTION_STYLE = true;
@@ -22,20 +23,38 @@ public class ProjectManager {
   public static final String KEY_BORDER_HEXCOLOR = "gui.border.hex_color";
   public static final String KEY_VOLUME_SLIDER_BG_HEX_COLOR = "gui.volumeslider_bg.hex_color";
   public static final String KEY_VOLUME_SLIDER_FG_HEX_COLOR = "gui.volumeslider_fg.hex_color";
+  public static final String KEY_VOLUME_SLIDER_MUTED_FG_HEX_COLOR = "gui.volumeslider_muted_fg.hex_color";
+  public static final String KEY_VOLUME_SLIDER_WARNING_FG_HEX_COLOR = "gui.volumeslider_warning.hex_color";
+  public static final String KEY_VOLUME_SLIDER_HEARING_LOSS_FG_HEX_COLOR = "gui.volumeslider_hearing_loss.hex_color";
   public static final String KEY_PROGRESS_SLIDER_NORMAL_HEX_COLOR = "gui.progressslider_normal.hex_color";
   public static final String KEY_AIE_MAJOR_TAG_HEX_COLOR = "gui.aie_major_tag.hex_color";
   public static final String KEY_AIE_MINOR_TAG_HEX_COLOR = "gui.aie_minor_tag.hex_color";
 
+  public static final String VALUE_WAVEFORM_LOWER_X_HEXCOLOR = "#BBC4EE";
+  public static final String VALUE_WAVEFORM_UPPER_X_HEXCOLOR = "#6B75DB";
+  public static final String VALUE_BORDER_HEXCOLOR = "#BBC4EE";
+  public static final String VALUE_VOLUME_SLIDER_BG_HEX_COLOR = "#BBC4EE";
+  public static final String VALUE_VOLUME_SLIDER_FG_HEX_COLOR = "#6956F5";
+  public static final String VALUE_VOLUME_SLIDER_NORMAL_HEX_COLOR = "#5D6AF5";
+  public static final String VALUE_AIE_MAJOR_TAG_HEX_COLOR = "#6c32c9";
+  public static final String VALUE_AIE_MINOR_TAG_HEX_COLOR = "#7f32fa";
+  public static final String VALUE_VOLUME_SLIDER_WARNING_FG_HEX_COLOR = "#ffaa2b";
+  public static final String VALUE_VOLUME_SLIDER_MUTED_FG_HEX_COLOR = "#696969";
+  public static final String VALUE_VOLUME_SLIDER_HEARING_LOSS_FG_HEX_COLOR = "#fa2d2d";
+
   public static Map<String, String> getDefaultPropertiesCustomization() {
     Map<String, String> properties = new HashMap<>();
-    properties.put(KEY_WAVEFORM_LOWER_X_HEXCOLOR, "#BBC4EE");
-    properties.put(KEY_WAVEFORM_UPPER_X_HEXCOLOR, "#6B75DB");
-    properties.put(KEY_BORDER_HEXCOLOR, "#BBC4EE");
-    properties.put(KEY_VOLUME_SLIDER_BG_HEX_COLOR, "#BBC4EE");
-    properties.put(KEY_VOLUME_SLIDER_FG_HEX_COLOR, "#6956F5");
-    properties.put(KEY_PROGRESS_SLIDER_NORMAL_HEX_COLOR, "#5D6AF5");
-    properties.put(KEY_AIE_MAJOR_TAG_HEX_COLOR, "#6c32c9");
-    properties.put(KEY_AIE_MINOR_TAG_HEX_COLOR, "#7f32fa");
+    properties.put(KEY_WAVEFORM_LOWER_X_HEXCOLOR, VALUE_WAVEFORM_LOWER_X_HEXCOLOR);
+    properties.put(KEY_WAVEFORM_UPPER_X_HEXCOLOR, VALUE_WAVEFORM_UPPER_X_HEXCOLOR);
+    properties.put(KEY_BORDER_HEXCOLOR, VALUE_BORDER_HEXCOLOR);
+    properties.put(KEY_VOLUME_SLIDER_BG_HEX_COLOR, VALUE_VOLUME_SLIDER_BG_HEX_COLOR);
+    properties.put(KEY_VOLUME_SLIDER_FG_HEX_COLOR, VALUE_VOLUME_SLIDER_FG_HEX_COLOR);
+    properties.put(KEY_PROGRESS_SLIDER_NORMAL_HEX_COLOR, VALUE_VOLUME_SLIDER_NORMAL_HEX_COLOR);
+    properties.put(KEY_AIE_MAJOR_TAG_HEX_COLOR, VALUE_AIE_MAJOR_TAG_HEX_COLOR);
+    properties.put(KEY_AIE_MINOR_TAG_HEX_COLOR, VALUE_AIE_MINOR_TAG_HEX_COLOR);
+    properties.put(KEY_VOLUME_SLIDER_WARNING_FG_HEX_COLOR, VALUE_VOLUME_SLIDER_WARNING_FG_HEX_COLOR);
+    properties.put(KEY_VOLUME_SLIDER_MUTED_FG_HEX_COLOR, VALUE_VOLUME_SLIDER_MUTED_FG_HEX_COLOR);
+    properties.put(KEY_VOLUME_SLIDER_HEARING_LOSS_FG_HEX_COLOR, VALUE_VOLUME_SLIDER_HEARING_LOSS_FG_HEX_COLOR);
     return properties;
   }
 
@@ -49,6 +68,8 @@ public class ProjectManager {
     properties.put(KEY_PROGRESS_SLIDER_NORMAL_HEX_COLOR, new String[] {});
     properties.put(KEY_AIE_MAJOR_TAG_HEX_COLOR, new String[] {});
     properties.put(KEY_AIE_MINOR_TAG_HEX_COLOR, new String[] {});
+    properties.put(KEY_VOLUME_SLIDER_WARNING_FG_HEX_COLOR, new String[] {});
+    properties.put(KEY_VOLUME_SLIDER_MUTED_FG_HEX_COLOR, new String[] {});
     return properties;
   }
 }
